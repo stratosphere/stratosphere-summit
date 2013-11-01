072f6cb7ef12ffe6421ed5ab93d20d332036fd15
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2013-11-01 13:50:56 +0100

    Add scala solution

--- exercise2/src/main/scala/eu/stratosphere/tutorial/Task4.scala
+++ exercise2/src/main/scala/eu/stratosphere/tutorial/Task4.scala
@@ -20,27 +20,54 @@ class Task4 extends PlanAssembler with PlanAssemblerDescription with Serializabl
     
     val source = TextFile(inputPath)
     
-    // Copy this from task 1
+    // This part is from Task1
     val termOccurences = source flatMap { line =>
+      val Array(docId, doc) = line.split(",")
+      doc.toLowerCase()
+        .split("""\W+""")
+        .filter { !Util.STOP_WORDS.contains(_) }
+        .toSet[String]
+        .map { w => (w, 1) }
     }
     
     val documentFrequencies = termOccurences
-      .groupBy {  }
-      .reduce {  }
+      .groupBy { case (w, _) => w }
+      .reduce { (w1, w2) => (w1._1, w1._2 + w2._2) }
     
-    // Copy this from task 2
+    // This part is from Task2
     val termFrequencies = source flatMap { line =>
+      val Array(docId, doc) = line.split(",")
+      doc.toLowerCase()
+        .split("""\W+""")
+        .filter { !Util.STOP_WORDS.contains(_) }
+        .foldLeft(new HashMap[String, Int]) { (map, word) =>
+          map get(word) match {
+            case Some(x) => map += (word -> (x+1))   //if in map already, increment count
+            case None => map += (word -> 1)          //otherwise, set to 1
+          }
+        }
+        .map { case (word, count) => (docId, word, count) }
     }
     
-    // Copy this from task 3
-    val  tfIdf = 
+    // This part is from Task3
+    val  tfIdf = documentFrequencies
+      .join(termFrequencies)
+      .where { case (w, _) => w }
+      .isEqualTo { case (_, w, _) => w }
+      .map { (left, right) =>
+        val (word, docFreq) = left
+        val (docId, _, termFreq) = right
+        (docId, word, termFreq * Math.log(Util.NUM_DOCUMENTS / docFreq))
+    }
       
-    // Here you should combine all the (docId, word, tf-idf) tuples for
-    // a document into a single WeightVector which has a docId and an
-    // Iterator of (term, tf-idf) tuples.
+    // This is Task4
     val tfIdfPerDocument = tfIdf
-      .groupBy {  }
+      .groupBy { case (doc, _, _) => doc }
       .reduceGroup { values =>
+        val buffered = values.buffered
+        val (docId, _, _) = buffered.head
+        val weightList = buffered map { t => (t._2, t._3) }
+        WeightVector(docId, weightList)
       }
     
     val sink = tfIdfPerDocument.write(outputPath, DelimitedDataSinkFormat(formatOutput _))
@@ -48,12 +75,12 @@ class Task4 extends PlanAssembler with PlanAssemblerDescription with Serializabl
     new ScalaPlan(Seq(sink))
   }
   
-  def formatOutput(t: (String, Iterator[(String, Double)])) = {
-    val terms = t._2 map { case (word, tfIdf) => word + ", " + tfIdf }
-    t._1 + ": " + terms.mkString("; ")
+  def formatOutput(w: WeightVector) = {
+    val terms = w.terms map { case (word, tfIdf) => word + ", " + tfIdf }
+    w.docId + ": " + terms.mkString("; ")
   }
   
-  case class WeightVector(docId: String, terms: Iterator[(String, Int)])
+  case class WeightVector(docId: String, terms: Iterator[(String, Double)])
 }
 
 object RunTask4 {
